2b682f2b28614b090cde306d7c77e6ef0eb64494
Лесняк Юліана <Yuliana.Lesniak@lnu.edu.ua>
2026-09-06 11:30:07 +0300

    lab01: виправлено деякі рядочки коду для коректного відображення кирилиці

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,14 +1,14 @@
 ﻿using System.Runtime.InteropServices;
 
 Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
-Console.WriteLine("Студентка: Лесняк Юліана, група ФЕІ-34");
+Console.WriteLine("Студентка: Лесняк Юлiана, група ФЕI-34");
 Console.WriteLine(new string('-', 52));
 Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
 Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
-Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
-Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+Console.WriteLine($"Архiтектура процесу : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версiя .NET (CLR) : {Environment.Version}");
 Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
 Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
 Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
 Console.WriteLine(new string('-', 52));
-Console.WriteLine("Предметна область: Бібліотека (Book, BookCopy, Reader, Loan)");
+Console.WriteLine("Предметна область: Бiблiотека (Book, BookCopy, Reader, Loan)");
